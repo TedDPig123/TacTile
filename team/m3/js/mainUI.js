04d08b401ee3d5b466f3f3314e35b086d0259e96
redblue64 <alinaalmonte2021@gmail.com>
2024-11-19 13:13:46 -0500

    Modifications from 11/18/2024. - Added Music folder     - Added background Music     - Added sound effects to opening side menu + 'create grid' button - Added Images folder     - Added background to website     - Added background image to side menu     - Changed the cursor to be a dagger - Added Font folder     - Added the DragonHunter Font
    * All of these are still work in progress. Main goals for the next update:
    - Add loading screen
    - Add sub side menu upon clicking option within side menu
    - Fix pointer (grabber)

--- team/m3/js/mainUI.js
+++ team/m3/js/mainUI.js
@@ -1,7 +1,32 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "270px";
-  }
+  document.getElementById("mySidenav").style.width = "19%";
+  playAudio('audio/Opening UI.mp3');
+}
+
+function openSubNav() {
+  document.getElementById("mySidenav").style.width = "39%"; //literally just making the existing ui even bigger 
+  playAudio('audio/Opening UI.mp3');
+}
   
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  playAudio('audio/Opening UI.mp3');
+}
+
+function playAudio(url) {
+  new Audio(url).play();
+}
+
+//https://stackoverflow.com/questions/6121203/how-to-do-fade-in-and-fade-out-with-javascript-and-css
+function fadeOut(element) {
+  var op = 1;  // initial opacity
+  var timer = setInterval(function () {
+      if (op <= 0.1){
+          clearInterval(timer);
+          element.style.display = 'none';
+      }
+      element.style.opacity = op;
+      element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+      op -= op * 0.1;
+  }, 50);
+}
