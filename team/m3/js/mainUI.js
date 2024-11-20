2ab0cd9e1dc692909f1cd94c636355d4cb114b29
Emily Shaw <chengyushaw@gmail.com>
2024-11-20 15:29:16 -0500

    Revert "Modifications from 11/18/2024."
    This reverts commit 04d08b401ee3d5b466f3f3314e35b086d0259e96.
    
    This is to get rid of change that was not in pull request incase it cause any error

--- team/m3/js/mainUI.js
+++ team/m3/js/mainUI.js
@@ -1,32 +1,7 @@
 function openNav() {
-  document.getElementById("mySidenav").style.width = "19%";
-  playAudio('audio/Opening UI.mp3');
-}
-
-function openSubNav() {
-  document.getElementById("mySidenav").style.width = "39%"; //literally just making the existing ui even bigger 
-  playAudio('audio/Opening UI.mp3');
-}
+    document.getElementById("mySidenav").style.width = "270px";
+  }
   
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  playAudio('audio/Opening UI.mp3');
-}
-
-function playAudio(url) {
-  new Audio(url).play();
-}
-
-//https://stackoverflow.com/questions/6121203/how-to-do-fade-in-and-fade-out-with-javascript-and-css
-function fadeOut(element) {
-  var op = 1;  // initial opacity
-  var timer = setInterval(function () {
-      if (op <= 0.1){
-          clearInterval(timer);
-          element.style.display = 'none';
-      }
-      element.style.opacity = op;
-      element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-      op -= op * 0.1;
-  }, 50);
-}
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
