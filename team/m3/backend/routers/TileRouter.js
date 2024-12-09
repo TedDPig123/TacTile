b05132ebd0ddceab62d23c4e245f30a7578efec6
TedDPig_git <85619008+TedDPig123@users.noreply.github.com>
2024-12-08 22:56:22 -0500

    tile-database #84: added more routes

--- team/m3/backend/routers/TileRouter.js
+++ team/m3/backend/routers/TileRouter.js
@@ -5,6 +5,8 @@ import {
     getTileByID,
     updateTile,
     deleteTile,
+    changeTileID,
+    clearAllTiles
 } from "../controllers/TileController.js"
 
 const tileRouter = new express.Router();
@@ -14,5 +16,8 @@ tileRouter.get('/', getAllTiles);
 tileRouter.get('/:id', getTileByID);
 tileRouter.put('/:id', updateTile);
 tileRouter.delete('/:id', deleteTile);
+tileRouter.put('/:id/change-id', changeTileID);
+tileRouter.delete('/clear', clearAllTiles);
+
 
 export default tileRouter;
